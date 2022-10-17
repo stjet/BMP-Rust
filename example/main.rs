9f857a69a00729d413e48e8800a904ae5b67e1bd
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2022-10-17 23:26:18 +0000

    change into crate (library)?

--- example/main.rs
+++ example/main.rs
@@ -1,8 +1,9 @@
+#[path = "../src/lib.rs"]
 mod bmp;
-use crate::bmp::BMP;
+use bmp::bmp::BMP;
 
 fn main() {
-  let file = BMP::new_from_file("src/images/example.bmp");  //tests of bmp lib
+  let file = BMP::new_from_file("example/images/example.bmp");  //tests of bmp lib
   let file_size = file.get_size(true);
   println!("File size (bytes): {}", file_size);
   assert_eq!(file_size/1024, BMP::num_bytes_to_kilobytes(file_size));
@@ -16,7 +17,7 @@ fn main() {
   }
   println!("Smaller file opened");
   //pixel data seems to start from bottom left
-  let mut small_file = BMP::new_from_file("src/images/small_example.bmp");
+  let mut small_file = BMP::new_from_file("example/images/small_example.bmp");
 
   /*
   if let Ok(unwrapped_dib_header2) = small_file.get_dib_header() {
@@ -38,32 +39,32 @@ fn main() {
   small_file.change_color_of_pixel(10, 10, [233, 71, 255, 255]);
   //alpha test
   small_file.change_color_of_pixel(15, 15, [255, 126, 16, 0]);
-  small_file.save_to_new("src/images/e.bmp");
-  let mut small_file2 = BMP::new_from_file("src/images/e.bmp");
+  small_file.save_to_new("example/images/e.bmp");
+  let mut small_file2 = BMP::new_from_file("example/images/e.bmp");
   //fill color may not be right
   small_file2.fill_bucket([155, 42, 66, 255], 35, 40);
-  small_file2.save_to_new("src/images/e2.bmp");
+  small_file2.save_to_new("example/images/e2.bmp");
   println!("Draw line test");
   //bug: coords are WRONG
-  let mut small_file3 = BMP::new_from_file("src/images/small_example.bmp");
+  let mut small_file3 = BMP::new_from_file("example/images/small_example.bmp");
   small_file3.draw_line([233, 30, 99, 255], [5, 3], [5, 29]);
   small_file3.draw_line([233, 30, 99, 255], [15, 40], [5, 40]);
   small_file3.draw_line([233, 30, 99, 255], [1, 2], [40, 2]);
-  small_file3.save_to_new("src/images/line_test1.bmp");
-  let mut small_file4 = BMP::new_from_file("src/images/small_example.bmp");
+  small_file3.save_to_new("example/images/line_test1.bmp");
+  let mut small_file4 = BMP::new_from_file("example/images/small_example.bmp");
   small_file4.draw_line([233, 30, 99, 255], [15, 32], [8, 20]); //backwards test
   small_file4.draw_line([233, 30, 99, 255], [3, 3], [14, 12]);
   small_file4.draw_line([100, 65, 45, 255], [20, 20], [52, 52]);
-  small_file4.save_to_new("src/images/line_test2.bmp");
-  let mut small_file5 = BMP::new_from_file("src/images/small_example.bmp");
+  small_file4.save_to_new("example/images/line_test2.bmp");
+  let mut small_file5 = BMP::new_from_file("example/images/small_example.bmp");
   small_file5.draw_line([125, 125, 170, 255], [4, 7], [8, 9]);
   small_file5.draw_line([255, 255, 255, 255], [8, 25], [40, 29]);
   small_file5.draw_line([0, 255, 0, 255], [8, 43], [40, 44]);
   small_file5.draw_line([0, 120, 11, 255], [35, 2], [36, 12]);
   small_file5.draw_line([100, 65, 45, 255], [4, 20], [7, 37]);
-  small_file5.save_to_new("src/images/line_test3.bmp");
+  small_file5.save_to_new("example/images/line_test3.bmp");
   //IS THIS LOSS.JPG? NOPE ITS LOSS.BMP
-  let mut small_file6 = BMP::new_from_file("src/images/small_example.bmp");
+  let mut small_file6 = BMP::new_from_file("example/images/small_example.bmp");
   small_file6.draw_line([255, 255, 255, 255], [10, 5], [10, 20]);
   small_file6.draw_line([255, 255, 255, 255], [30, 5], [30, 20]);
   small_file6.draw_line([255, 255, 255, 255], [40, 10], [40, 20]);
@@ -71,22 +72,22 @@ fn main() {
   small_file6.draw_line([255, 255, 255, 255], [20, 35], [20, 45]);
   small_file6.draw_line([255, 255, 255, 255], [40, 35], [40, 45]);
   small_file6.draw_line([255, 255, 255, 255], [35, 42], [50, 42]);
-  small_file6.save_to_new("src/images/loss.bmp");
+  small_file6.save_to_new("example/images/loss.bmp");
   println!("Draw rect test");
-  let mut small_file7 = BMP::new_from_file("src/images/small_example.bmp");
+  let mut small_file7 = BMP::new_from_file("example/images/small_example.bmp");
   small_file7.draw_rectangle(None, Some([255, 255, 255, 255]), [0,2], [15,11]);
   //problems here, outline not drawn correctly
   small_file7.draw_rectangle(Some([0, 0, 0, 255]), Some([255, 255, 255, 255]), [40, 7], [45, 19]);
-  small_file7.save_to_new("src/images/rect_test.bmp");
+  small_file7.save_to_new("example/images/rect_test.bmp");
   //invert test
   println!("Invert test");
-  let mut small_file8 = BMP::new_from_file("src/images/small_example.bmp");
+  let mut small_file8 = BMP::new_from_file("example/images/small_example.bmp");
   small_file8.invert(None);
-  small_file8.save_to_new("src/images/invert_test.bmp");
+  small_file8.save_to_new("example/images/invert_test.bmp");
   //new file test
   println!("New file test");
   let mut new_file = BMP::new(15, 15);
   let new_file_header = new_file.get_header();
   assert_eq!(138, new_file_header.bfOffBits);
-  new_file.save_to_new("src/images/artificial.bmp");
+  new_file.save_to_new("example/images/artificial.bmp");
 }
