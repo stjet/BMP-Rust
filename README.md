ec0931448cdecd0ab31078443f381ff27c5db06d
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2022-11-24 18:18:09 +0000

    docs

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 BMP Rust is a rust library to read and write .bmp Image files. It has zero dependencies.
 
 ## Install
-First add bmp-rust to your Cargo.toml file
+Obviously, install Rust. Add bmp-rust to your Cargo.toml file
 
 ```toml
 [dependencies]
-bmp-rust ="0.2.3"
+bmp-rust ="0.2.4"
 ```
 
 You can now use the crate:
@@ -14,3 +14,27 @@ You can now use the crate:
 ```rust
 use bmp_rust::bmp::BMP;
 ```
+
+## Documentation
+First, load a BMP file by file path, or create a new one:
+```rust
+let mut bmp_from_file = BMP::new_from_file("midnight.bmp");
+let mut bmp_from_scratch = BMP::new(15, 15, None);
+```
+
+Information can now be read from the file:
+```rust
+let file_size = bmp_from_file.get_size(true);
+let dib_header = bmp_from_file.get_dib_header().unwrap();
+let width = dib_header.width;
+let height = dib_header.height;
+let pixel_color = bmp_from_file.get_color_of_px(10, 10).unwrap();
+```
+
+Or new pixel data can be written to it:
+```rust
+```
+
+Look at the [source code](src/bmp.rs) or [tests/example](example/main.rs) for more functions, and their usage.
+
+[docs.rs](https://docs.rs/bmp-rust/0.2.4/bmp_rust/bmp/index.html)
--- example/ext.rs
+++ example/ext.rs
@@ -1,28 +1,50 @@
+#[path = "../src/lib.rs"]
+mod bmp;
+use bmp::bmp::BMP;
 use std::process::Command;
 
 //layer
 pub struct VideoLayer {
-  fn new(width: u16, height: u16) {
-    //
-  }
+  width: u16,
+  height: u16,
+  pub frames: Vec<BMP>,
 }
 
 impl VideoLayer {
-  //
+  fn new(width: u16, height: u16) {
+    return VideoLayer{
+      width: width,
+      height: height,
+      frames: vec![],
+    };
+  }
+  fn add_frame(&mut self, frame: BMP) {
+    self.frames.push(frame);
+  }
+  fn add_empty_frame(&mut self) {
+    self.frames.push(BMP::new(self.width, self.height, [0, 0, 0, 0]));
+  }
 }
 
 //video with export with ffmpeg
 pub struct VideoConstructor {
-  //
+  pub layers: Vec<VideoLayer>,
 }
 
 impl VideoConstructor {
   fn new(width: u16, height: u16, layer_num: u8, fps: u8) {
-    //
+    let layers_vec: Vec<VideoLayer> = vec![];
+    for i in 0..layer_num {
+      layers_vec.push(VideoLayer::new(width, height));
+    }
+    return VideoConstructor {
+      layers: layers_vec,
+    };
   }
-  //
   fn export(&self) {
+    //loop through self.layers, composite layers frames
     //use ffmpeg to convert to video?
     //Command::new("ffmpeg").args([])
+    //maybe opencv
   }
 }
